ef34c15986fc1bcda62260bc2e0363427282ea75
Camilo Juan <cjuan@cipfpbatoi.es>
2018-11-29 02:01:11 +0100

    Tarea 5

---
+++ Calculator.php
@@ -0,0 +1,25 @@
+<?php
+class Calculator
+{
+ 
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function division($a, $b)
+    {
+        return $a / $b;
+    }
+
+    public function esPar($a, $b)
+    {
+        return $a % $b;
+    }
+ 
+}
